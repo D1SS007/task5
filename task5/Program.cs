80fc441dde77ada3f0a44eb703cf22c19ea66e54
D1SS <D1SS@DESKTOP-2QR7GQG>
2022-03-18 18:44:14 +0300

    Добавьте файлы проекта.

---
+++ task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Иван";
+            string mySecondName = "Иванов";
+            
+
+
+            Console.WriteLine("До перестановки = " + myName + " " + mySecondName);
+
+            Console.ReadKey();
+
+            Console.WriteLine("После перестановки " + mySecondName + " " + myName );
+
+           
+        }
+    }
+}
